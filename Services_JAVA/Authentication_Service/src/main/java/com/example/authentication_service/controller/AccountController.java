604d9be9e19ea029753a52b736b95e4eb2349712
PhamHinh2004 <20102004hinh@gmail.com>
2026-06-15 16:36:03 +0700

    finish schema and some first steps security for system

--- Services_JAVA/Authentication_Service/src/main/java/com/example/authentication_service/controller/AccountController.java
+++ Services_JAVA/Authentication_Service/src/main/java/com/example/authentication_service/controller/AccountController.java
@@ -3,19 +3,15 @@ package com.example.authentication_service.controller;
 import com.example.authentication_service.exception.RoleNotFoundException;
 import com.example.authentication_service.exception.UserAlreadyExistsException;
 import com.example.authentication_service.mapper.AccountMapper;
-import com.example.authentication_service.models.dto.request.AccountLogin;
-import com.example.authentication_service.models.dto.request.AccountRequest;
 import com.example.authentication_service.models.dto.request.AccountSignIn;
 import com.example.authentication_service.models.dto.request.AccountSignUp;
 import com.example.authentication_service.models.dto.response.AccountResponse;
+import com.example.authentication_service.models.dto.response.ApiResponseDto;
 import com.example.authentication_service.service.AccountService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/account")
@@ -23,19 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
     private final AccountService accountService;
     private final AccountMapper accountMapper;
-
-
-    @PostMapping("/register")
-    public ResponseEntity<AccountResponse> registerAccount(@RequestBody @Valid AccountRequest accountRequest) {
-        AccountResponse account_response =  accountMapper.toAccountResponse(accountService.saveAccount(accountRequest));
-        return ResponseEntity.ok(account_response);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<Boolean> loginAccount(@RequestBody AccountLogin accountLogin) {
-        return ResponseEntity.ok(accountService.checkPassword(accountLogin.getUsername(), accountLogin.getPassword()));
-    }
-
     @PostMapping("/auth/register")
     public ResponseEntity<?> signUp(@RequestBody @Valid AccountSignUp accountSignUp) throws RoleNotFoundException, UserAlreadyExistsException {
         return accountService.signUp(accountSignUp);
@@ -46,4 +29,13 @@ public class AccountController {
         return accountService.signIn(accountSignIn);
     }
 
+    @GetMapping("/check-username")
+    public ResponseEntity<ApiResponseDto<Boolean>> checkUsername(@RequestParam("username") String username){
+        return ResponseEntity.ok(new ApiResponseDto<>("Check username", "success", accountService.checkExistUsername(username)));
+    }
+
+    @GetMapping("/check-email")
+    public ResponseEntity<?> checkEmail(@RequestParam("email") String email){
+        return ResponseEntity.ok(accountService.checkExistEmail(email));
+    }
 }
--- Services_JAVA/Authentication_Service/src/main/java/com/example/authentication_service/service/RoleService.java
+++ Services_JAVA/Authentication_Service/src/main/java/com/example/authentication_service/service/RoleService.java
@@ -1,4 +1,36 @@
 package com.example.authentication_service.service;
 
+import com.example.authentication_service.exception.AppException;
+import com.example.authentication_service.exception.ErrorCode;
+import com.example.authentication_service.mapper.RoleMapper;
+import com.example.authentication_service.models.Role;
+import com.example.authentication_service.models.dto.response.RoleResponse;
+import com.example.authentication_service.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class RoleService {
+    private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
+
+
+    public List<RoleResponse> findAll(){
+        List<Role> roles = roleRepository.findAll();
+        List<RoleResponse> roleResponses = new ArrayList<>();
+        for (Role role : roles) {
+            RoleResponse roleResponse = roleMapper.toRoleResponse(role);
+            roleResponses.add(roleResponse);
+        }
+        if(roleResponses.isEmpty()){
+            throw new AppException(ErrorCode.NOT_FOUND);
+        }
+        return roleResponses;
+    }
+
+
 }
