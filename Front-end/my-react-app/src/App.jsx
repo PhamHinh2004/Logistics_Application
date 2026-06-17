32ec7187cf041b90eec32f0a598d332cab42bf34
PhamHinh2004 <20102004hinh@gmail.com>
2026-06-18 01:43:56 +0700

    update UX-UI login page and register page

--- Front-end/my-react-app/src/App.jsx
+++ Front-end/my-react-app/src/App.jsx
@@ -1,10 +1,15 @@
 import RegisterPage from './authentication/RegisterPage'
-
+import LoginPage from './authentication/LoginPage'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './App.css'
 function App() {
   return (
-    <div className="App">
-      <RegisterPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
