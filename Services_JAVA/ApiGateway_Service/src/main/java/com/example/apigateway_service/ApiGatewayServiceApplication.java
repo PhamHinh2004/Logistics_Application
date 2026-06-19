49a00fab7ddd4eb5b2b99be496be4d08862eb731
PhamHinh2004 <20102004hinh@gmail.com>
2026-06-19 23:27:16 +0700

    generate customer service and api gate way service

--- Services_JAVA/ApiGateway_Service/src/main/java/com/example/apigateway_service/ApiGatewayServiceApplication.java
+++ Services_JAVA/ApiGateway_Service/src/main/java/com/example/apigateway_service/ApiGatewayServiceApplication.java
@@ -1,16 +1,17 @@
-package com.example.discovery_service;
+package com.example.apigateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableEurekaServer
-
-public class DiscoveryServiceApplication {
+public class ApiGatewayServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServiceApplication.class, args);
+        SpringApplication.run(ApiGatewayServiceApplication.class, args);
     }
 
 }
