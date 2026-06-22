6d33c0cb527a71f03fa87ababd23d05232cb947a
PhamHinh2004 <20102004hinh@gmail.com>
2026-06-23 00:56:04 +0700

    change form sigin response but fail in deploying production in docker

--- Services_JAVA/Authentication_Service/src/main/java/com/example/authentication_service/models/dto/response/SignInResponse.java
+++ Services_JAVA/Authentication_Service/src/main/java/com/example/authentication_service/models/dto/response/SignInResponse.java
@@ -1,5 +1,6 @@
 package com.example.authentication_service.models.dto.response;
 
+import com.example.authentication_service.models.StatusAccount;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,4 +17,7 @@ public class SignInResponse {
     private String username;
     private String email;
     private List<String> roles;
+    private boolean isActive;
+    private String phone;
+    private StatusAccount statusAccount;
 }
