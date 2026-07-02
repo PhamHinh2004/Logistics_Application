fd24aa63ef4b53275aff448d6a5fe7aa97791737
PhamHinh2004 <20102004hinh@gmail.com>
2026-07-03 01:43:23 +0700

    created fleet_service c#

---
+++ Services/Fleet_Service/Enums/ContainerStatus.cs
@@ -0,0 +1,12 @@
+namespace Fleet_Service.Enums
+{
+    public enum ContainerStatus
+    {
+        AVAILABLE,
+        LOADED,
+        IN_TRANSIT,
+        EMPTY,
+        MAINTENANCE,
+        DAMAGED
+    }
+}
